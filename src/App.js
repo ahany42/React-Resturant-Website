f4f7cc1b461e53e12ddcb095a7c7e28c3ea3b556
Ahany42 <ahanyfathy42@gmail.com>
2024-01-05 23:14:53 +0200

    responsive homescreen p1

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import image3 from './assets/img3.jpg';
 import pizzaGif from './assets/pizza-ad.gif';
 import { FaGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-
-
+import { IoMdMail } from "react-icons/io";
+<meta content="width=device-width, initial-scale=1" name="viewport" />
 
 export default  function App() {
   
@@ -88,9 +88,11 @@ return <div className="HomePageContainer">
     <h6 className="footercomp">+20 1062020784</h6>
     <h6 className="footercomp">Heliopolis,Cairo,Egypt</h6>
     <h6 className="footercomp">&copy; All Copy Rights Reserved Master Cheif Resturant</h6>
-     <div className="footercomp">
-     <FaGithub />
-     <FaLinkedin />
+     <div className="socialcontainer">
+     <a href="https://github.com/ahany42/" target="_blank"><FaGithub /></a>
+     <a href="https://www.linkedin.com/in/aly-hany-261275219/" target="_blank"><FaLinkedin /></a>
+     <a href="mailto:ahanyfathy42@gmail.com" target="_blank"><IoMdMail /></a>
+   
      </div>
     <footer>
   
