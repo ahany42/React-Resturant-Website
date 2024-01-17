d331fdbff744fae3a5cec4b8dcf76fab544de767
Ahany42 <ahanyfathy42@gmail.com>
2024-01-17 13:58:35 +0200

    Homepage carousel contnent component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,28 @@
 import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 import img3 from './assets/img3.jpg';
+import img1 from './assets/img1.png';
+function HomeCarouselContent({img,title,caption}){
+return <div className="CarouselContent">
+  <div className="CarouselContainer">
+  <h2 className="CarouselTitle">{title}</h2>
+  <div className="CarouselImgContainer">
+   <img className="Carouselimg" src={img}></img>
+   <h5 className="CarouselCaption">{caption}</h5>
+  </div>
+
+</div>
+<div className="buttonContainer">
+<button type="button" class="btn btn-outline-dark">Order Now</button>
+</div>
+</div>
+}
 function HomepageCarousel(){
-    return (  <Carousel className="Carousel" data-bs-theme="light">
+    return (  <Carousel className="Carousel" data-bs-theme="dark">
     <Carousel.Item>
       <img 
-        className="Carouselimg"
+       
         src={img3}
         alt="First slide"
       />
@@ -12,19 +30,14 @@ function HomepageCarousel(){
     </Carousel.Item>
     <Carousel.Item>
       <img
-        className="Carouselimg"
+      
         src={img3}
         alt="Second slide"
       />
   
     </Carousel.Item>
     <Carousel.Item>
-      <img
-        className="Carouselimg"
-        src={img3}
-        alt="Third slide"
-      />
-  
+    <HomeCarouselContent caption="We believe in quality, and that starts with our ingredients. Savor the goodness of locally-sourced, fresh produce and premium cheeses that elevate our pizzas to a whole new level." title="Order The Best Pizza In Town" img={img1}/>
     </Carousel.Item>
   </Carousel>
       );
