31c131f2d0d839b54790cee8f1da60a32c8e54e5
Ahany42 <ahanyfathy42@gmail.com>
2024-01-29 11:41:49 +0200

    reservation calendar

--- src/Components/ReservePage.js
+++ src/Components/ReservePage.js
@@ -1,9 +1,15 @@
 import { HiMiniInformationCircle } from "react-icons/hi2";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
+
 function ReservePage(){
  return(
  <div className="ReservePage"> 
   <div class="alert alert-info" role="alert">
   <HiMiniInformationCircle /> Reserve through the official website
+</div>
+<div className="calendarContainer">
+<Calendar/>
 </div>
  </div>
  );
